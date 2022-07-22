84e614ad6d28dbfbb0144aefcab77f56a2811fb2
misaka <1784321894@qq.com>
2022-07-23 00:26:37 +0800

    Proj01 : Problem 04 -u

--- Proj/hog/hog.py
+++ Proj/hog/hog.py
@@ -112,8 +112,7 @@ def take_turn(num_rolls, player_score, opponent_score, dice=six_sided, goal=GOAL
     if num_rolls == 0:
         return hefty_hogs(player_score, opponent_score)
     
-    return roll_dice(num_rolls, dice)
-        
+    return roll_dice(num_rolls, dice)    
     # END PROBLEM 3
 
 
