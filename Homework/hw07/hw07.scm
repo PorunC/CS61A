390617eec331b7be32c6b2e078f6030615209f87
misaka <1784321894@qq.com>
2022-07-28 10:12:43 +0800

    hw07 : kwlist_construct

--- Homework/hw07/hw07.scm
+++ Homework/hw07/hw07.scm
@@ -2,26 +2,42 @@
 
 (define (make-kwlist1 keys values)
   'YOUR-CODE-HERE
+  (list keys values)
 )
 
 (define (get-keys-kwlist1 kwlist) 
   'YOUR-CODE-HERE
+  (car kwlist)
 )
 
 (define (get-values-kwlist1 kwlist)
   'YOUR-CODE-HERE
+  (cadr kwlist)
 )
 
 (define (make-kwlist2 keys values)
   'YOUR-CODE-HERE
+  (if (null? keys)
+    nil
+    (cons
+      (list (car keys) (car values))
+      (make-kwlist2 (cdr keys) (cdr values))
+    )
+  )
 )
 
 (define (get-keys-kwlist2 kwlist) 
   'YOUR-CODE-HERE
+  (map
+    (lambda (x) (car x)) kwlist
+  )
 )
 
 (define (get-values-kwlist2 kwlist)
   'YOUR-CODE-HERE
+  (map
+    (lambda(x) (cadr x)) kwlist
+  )
 )
 
 (define (add-to-kwlist kwlist key value)
